6212d567a22e4c7c1824557a8a7628db3d2051bc
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-03-12 14:35:09 +0200

    Create Databasehelper class to save new notes to SQLite.
    Create a Databasehelper class that uses SQLiteOpenHelper to save created notes to a database when the add button is pressed in the NewNoteActivity.

--- app/src/main/java/fi/example/fancynotes/NewNoteActivity.java
+++ app/src/main/java/fi/example/fancynotes/NewNoteActivity.java
@@ -3,18 +3,49 @@ package fi.example.fancynotes;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewNoteActivity extends AppCompatActivity {
+    DatabaseHelper mDatabaseHelper;
+    private Button addBtn;
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newnote);
+        addBtn = (Button) findViewById(R.id.addButton);
+        editText = (EditText) findViewById(R.id.newNoteEditText);
+        mDatabaseHelper = new DatabaseHelper(this);
 
+    }
+
+
+    public void addNote(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if(insertData) {
+            toastMessage("Data successfully Inserted");
+        } else {
+            toastMessage("Something went wrong!");
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 
     public void addNewNote(View v) {
         Log.d("NewNoteActivityAdd","works");
+        String newEntry = editText.getText().toString();
+        if(editText.length() != 0) {
+            addNote(newEntry);
+        } else {
+            toastMessage("You must put something in the note field.");
+        }
     }
 }
