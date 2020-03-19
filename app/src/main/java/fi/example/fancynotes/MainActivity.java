cea46a48cd3a3382eb20b002b315eaeeb4d4ceca
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-03-19 11:30:21 +0200

    Add button and textview for test fetching.

--- app/src/main/java/fi/example/fancynotes/MainActivity.java
+++ app/src/main/java/fi/example/fancynotes/MainActivity.java
@@ -7,17 +7,23 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     DatabaseHelper mDatabaseHelper;
+    TextView text;
+    Button fetch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDatabaseHelper = new DatabaseHelper(this);
+        text = findViewById(R.id.fetchingText);
+        fetch = findViewById(R.id.fetchButton);
         System.out.println("Moi Hanski");
         System.out.println("Moi Maija");
     }
@@ -30,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
     public void seeAllNotes(View v) {
         Intent i = new Intent(this, CardViewActivity.class);
         startActivity(i);
+    }
+
+    public void onClick(View view) {
+
     }
 
 }
