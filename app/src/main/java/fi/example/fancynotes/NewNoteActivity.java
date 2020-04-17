2a1eb029771dbd4921c2a51a871b5403b729ec79
Maija <maija.visala@tuni.fi>
2020-04-17 16:05:21 +0300

    Redesign new note activity.

--- app/src/main/java/fi/example/fancynotes/NewNoteActivity.java
+++ app/src/main/java/fi/example/fancynotes/NewNoteActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
