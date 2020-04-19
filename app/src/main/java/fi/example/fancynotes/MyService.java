a0720e306468a7e00414f007b57c4c2a20e1b5c2
Maija <maija.visala@tuni.fi>
2020-04-19 20:07:47 +0300

    Add javadoc for MyService.

--- app/src/main/java/fi/example/fancynotes/MyService.java
+++ app/src/main/java/fi/example/fancynotes/MyService.java
@@ -2,40 +2,52 @@ package fi.example.fancynotes;
 
 import android.app.Service;
 import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.util.JsonReader;
-import android.util.Log;
-import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * Service for fetching notes that are added via web app.
+ * Service fetches a json array from web app backend
+ *
+ * @author  Hanna Tuominen
+ * @author  Maija Visala
+ * @version 3.0
+ * @since   2020-03-09
+ */
 public class MyService extends Service {
     Thread t;
     long id;
     String title;
     String  text;
     DatabaseHelper mDatabaseHelper;
+
+    /**
+     * Mandatory method for service.
+     * @param intent mandatory parameter for method.
+     * @return null.
+     */
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+    /**
+     * When service starts, create new thread that performs wanted tasks.
+     * @param i mandatory parameter for method.
+     * @param flags mandatory parameter for method.
+     * @param startId mandatory parameter for method.
+     * @return
+     */
     public int onStartCommand(Intent i, int flags, int startId){
-        Log.d("jepajee", "jepajee");
         mDatabaseHelper = new DatabaseHelper(this);
         t = new Thread(() -> {
             doThis();
@@ -45,6 +57,10 @@ public class MyService extends Service {
         return START_STICKY;
     }
 
+    /**
+     * Main task of the service. Get data from web app backend and convert it to json objects.
+     * Add new note data to SQLite.
+     */
     public void doThis(){
         String value;
         JSONObject jsonObject;
@@ -59,7 +75,6 @@ public class MyService extends Service {
 
                 mDatabaseHelper.addData(Util.getNewOrderId(this), title, text,"note_placeholder2",null,"No Audio",null,null);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         } catch (JSONException e) {
@@ -73,9 +88,14 @@ public class MyService extends Service {
         manager.sendBroadcast(i);
     }
 
+    /**
+     * Open connection to web app backend and fetch json string that contains
+     * data of the notes added via web app.
+     * @return string fetched from the web app backend.
+     * @throws IOException
+     */
     private String downloadUrl() throws IOException {
         InputStream in = null;
-
         try{
             URL url = new URL("https://fancynotes.herokuapp.com/getAll");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -86,7 +106,6 @@ public class MyService extends Service {
             while ((myChar = in.read()) != -1){
                 result += (char) myChar;
             }
-            Log.d("jepajee", result);
             return result;
         }finally {
             if(in != null){
